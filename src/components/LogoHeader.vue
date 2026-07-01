fb059aa7277cc13a247911300a57b4d65b9c3dca
Łukasz Jędrzejczyk <inz.lukasz.jedrzejczyk@gmail.com>
2026-07-02 00:49:44 +0200

    Initial CSS setup. LogoHeader component.

---
+++ src/components/LogoHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img src="@/assets/images/logo.svg" alt="FX Checker" />
+    <!-- Dynamic: number of available currencies, e.g. "55" -->
+    <div class="text-preset-4">
+      <span>55 Currencies</span>
+      <span>EOD</span>
+      <span>ECB data</span>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5rem;
+}
+div {
+  color: var(--neutral-200);
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: -0.5rem;
+}
+span {
+  display: block;
+  margin-right: 0.5rem;
+}
+span + span::before {
+  content: '·';
+  margin-right: 0.5rem;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+const app = createApp(App)
+app.use(PrimeVue, {
+  license: import.meta.env.VITE_PRIMEUI_LICENSE_KEY,
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark-mode',
+    },
+  },
+})
+app.mount('#app')
 
-createApp(App).mount('#app')
+console.log(import.meta.env.VITE_PRIMEUI_LICENSE_KEY)
